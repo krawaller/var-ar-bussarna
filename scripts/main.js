ac73ff9e8e267a32f64ccc71228dd7e85eedc283
Jacob Waller <jaco@spotify.com>
2014-05-08 21:02:34 +0200

    Enable retina detection

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,8 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  id: 'examples.map-9ijuk24y'
+  id: 'examples.map-9ijuk24y',
+  detectRetina: true
 }).addTo(map);
 
 function renderItem(item) {
