700588bc9ab77bfed6bf550a4a0bc9ef651c779f
Jacob Waller <jaco@spotify.com>
2014-05-29 09:10:28 +0200

    Unbreak Mapbox tile layer

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  id: 'examples.map-9ijuk24y',
+  id: 'litenjacob.ibnmhb49',
   detectRetina: true
 }).addTo(map);
 
